03feada8e1810a7dfd39feeb2f08b8bd9943351e
Nassim069 <im_aliousalah@esi.dz>
2021-10-06 18:30:28 +0100

    creating the helper file jwt_helper ( getJWTFromRequest + validateJWTFromRequest + getSignedJWTForUser)

---
+++ app/Helpers/jwt_helper.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\UserModel;
+use Config\Services;
+use Firebase\JWT\JWT;
+
+function getJWTFromRequest($authenticationHeader): string
+{
+    if (is_null($authenticationHeader)) {
+        throw new Exception('Missing or invalid JWT in request');
+    }
+    return explode(' ', $authenticationHeader)[1];
+}
+
+function validateJWTFromRequest(string $encodedToken)
+{
+    $key = Services::getSecretKey();
+    $decodedToken = JWT::decode($encodedToken, $key, ['HS256']);
+    $userModel = new UserModel();
+    $userModel->findUserByEmailAddress($decodedToken->email);
+}
+
+function getSignedJWTForUser(string $email)
+{
+    $issuedAtTime = time();
+    $tokenTimeToLive = getenv('JWT_TIME_TO_LIVE');
+    $tokenExpiration = $issuedAtTime + $tokenTimeToLive;
+    $payload = [
+        'email' => $email,
+        'iat' => $issuedAtTime,
+        'exp' => $tokenExpiration,
+    ];
+
+    $jwt = JWT::encode($payload, Services::getSecretKey());
+    return $jwt;
+}
