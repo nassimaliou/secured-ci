60bdc2a9967435bbd54b350522251267a2aa99c1
Nassim069 <im_aliousalah@esi.dz>
2021-10-06 20:24:44 +0100

    updating the $aliases and $filters arrays in the Filters.php file

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -19,6 +19,7 @@ class Filters extends BaseConfig
         'csrf'     => CSRF::class,
         'toolbar'  => DebugToolbar::class,
         'honeypot' => Honeypot::class,
+        'auth' => JWTAuthenticationFilter::class
     ];
 
     /**
@@ -58,5 +59,7 @@ class Filters extends BaseConfig
      *
      * @var array
      */
-    public $filters = [];
+    public $filters = [
+        'auth' => [ 'before' => ['client/*','client'] ]
+    ];
 }
