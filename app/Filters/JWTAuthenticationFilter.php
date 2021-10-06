ca6f025b82c56e91cba8012115b34eab2596ad4f
Nassim069 <im_aliousalah@esi.dz>
2021-10-06 20:36:05 +0100

    random commit

--- app/Filters/JWTAuthenticationFilter.php
+++ app/Filters/JWTAuthenticationFilter.php
@@ -16,24 +16,21 @@ class JWTAuthenticationFilter implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
         $authenticationHeader = $request->getServer('HTTP_AUTHORIZATION');
-
         try {
+
             helper('jwt');
             $encodedToken = getJWTFromRequest($authenticationHeader);
             validateJWTFromRequest($encodedToken);
             return $request;
-        }
-        catch (Exception $e) {
-            return Services::response()->setJson(
-                    [
-                        'error' => $e->getMessage()
-                    ]
-            )->setStatusCode(RequestInterface::HTTP_AUTHORIZATION);
+
+        } catch (Exception $e) {
+
+            return Services::response()->setJSON(['error' => $e->getMessage()])->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED);
         }
     }
 
-    public function after(RequestInterface $request,ResponseInterface $response,$arguments = null)
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-    }
 
+    }
 }
