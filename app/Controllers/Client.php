cbdff30296154a626b13f98b3de29702e8d25058
Nassim069 <im_aliousalah@esi.dz>
2021-10-07 14:58:16 +0100

    Creating Client controller Client.php with the index(), store(), show() functions

---
+++ app/Controllers/Client.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ClientModels;
+use CodeIgniter\HTTP\Response;
+use CodeIgniter\HTTP\ResponseInterface;
+use Exception;
+
+class Client extends BaseController
+{
+
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+}
