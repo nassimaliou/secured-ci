35bda5a5cccbbf1fcbcd68a009247ea377f9d4f9
Nassim069 <im_aliousalah@esi.dz>
2021-10-07 15:25:25 +0100

    implementing the index(), store(), and show($id) functions

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -9,20 +9,86 @@ use Exception;
 
 class Client extends BaseController
 {
-
+    /*
+        get all clients
+    */
     public function index()
     {
+        $model = new ClientModel();
+
+        return $this->getResponse(
+            [
+                'message' => 'Clients retrieved successfully',
+                'clients' => $model->findAll();
+            ]
+        );
 
     }
 
+
+
+    /*
+        add client
+    */
     public function store()
     {
+        $rules = [
+                    'name' => 'required',
+                    'email' => 'required|min_length[6]|max_length[50]|valid_email|is_unique[client.email]',
+                    'retainer_fee' => 'required|max_length[255]'
+                 ];
+
+        $input = $this->getRequestInput($this->request);
+
+        if (!$this->validateRequest($input, $rules)) {
+            return $this
+                ->getResponse(
+                    $this->validator->getErrors(),
+                    ResponseInterface::HTTP_BAD_REQUEST
+                );
+        }
+
+        $clientEmail = $input['email'];
+
+        $model = new ClientModel();
+        $model->save($input);
+
+        $client = $model->where('email', $clientEmail)->first();
+
+        return $this->getResponse(
+            [
+                'message' => 'Client added successfully',
+                'client' => $client
+            ]
+        );
 
     }
 
+    /*
+        get a single client by ID
+    */
     public function show($id)
     {
+        try {
+            $model = new ClientModel();
+            $client = $model->findClientById($id);
 
+            return $this->getResponse(
+                [
+                    'message' => 'Client retrieved successfully',
+                    'client' => $client
+                ]
+            );
+        }
+        catch(Exception $e) {
+            return $this->getResponse(
+                [
+                    'message' => 'Could not find client for specified ID'
+                ],
+                ResponseInterface::HTTP_NOT_FOUND
+            );
+        }
     }
+
 
 }
